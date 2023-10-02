005f64520ff76ee9dbcbd4fcd823609add3adf5b
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2023-10-03 00:20:48 +0200

    add customized mediator

---
+++ mediator/WsInboundTransport.ts
@@ -0,0 +1,130 @@
+import type {
+  Agent,
+  InboundTransport,
+  Logger,
+  TransportSession,
+  EncryptedMessage,
+  AgentContext,
+} from '@aries-framework/core'
+
+import { AriesFrameworkError, TransportService, utils, MessageReceiver } from '@aries-framework/core'
+import WebSocket, { Server } from 'ws'
+
+export class WsInboundTransport implements InboundTransport {
+  private socketServer: Server
+  private logger!: Logger
+
+  // We're using a `socketId` just for the prevention of calling the connection handler twice.
+  private socketIds: Record<string, unknown> = {}
+
+  public constructor({ server, port }: { server: Server; port?: undefined } | { server?: undefined; port: number }) {
+    this.socketServer = server ?? new Server({ port })
+  }
+
+  public async start(agent: Agent) {
+    const transportService = agent.dependencyManager.resolve(TransportService)
+
+    this.logger = agent.config.logger
+
+    const wsEndpoint = agent.config.endpoints.find((e) => e.startsWith('ws'))
+    this.logger.debug(`Starting WS inbound transport`, {
+      endpoint: wsEndpoint,
+    })
+
+    this.socketServer.on('connection', (socket: WebSocket) => {
+      const socketId = utils.uuid()
+      this.logger.debug('Socket connected.')
+
+      if (!this.socketIds[socketId]) {
+        this.logger.debug(`Saving new socket with id ${socketId}.`)
+        this.socketIds[socketId] = socket
+        const session = new WebSocketTransportSession(socketId, socket, this.logger)
+        this.listenOnWebSocketMessages(agent, socket, session)
+        socket.on('close', () => {
+          this.logger.debug('Socket closed.')
+          transportService.removeSession(session)
+        })
+      } else {
+        this.logger.debug(`Socket with id ${socketId} already exists.`)
+      }
+    })
+  }
+
+  public async stop() {
+    this.logger.debug('Closing WebSocket Server')
+
+    return new Promise<void>((resolve, reject) => {
+      this.socketServer.close((error) => {
+        if (error) {
+          reject(error)
+        }
+        resolve()
+      })
+    })
+  }
+
+  private listenOnWebSocketMessages(agent: Agent, socket: WebSocket, session: TransportSession) {
+    const messageReceiver = agent.dependencyManager.resolve(MessageReceiver)
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    socket.addEventListener('message', async (event: any) => {
+      this.logger.debug('WebSocket message event received.', { url: event.target.url })
+      try {
+        await messageReceiver.receiveMessage(JSON.parse(event.data), { session })
+      } catch (error) {
+        this.logger.error('Error processing message: ' + error)
+      }
+    })
+  }
+}
+
+export class WebSocketTransportSession implements TransportSession {
+  public id: string
+  public readonly type = 'WebSocket'
+  public socket: WebSocket
+  private logger!: Logger
+
+  public constructor(id: string, socket: WebSocket, logger: Logger) {
+    this.id = id
+    this.socket = socket
+    this.logger = logger
+  }
+
+  public async send(agentContext: AgentContext, encryptedMessage: EncryptedMessage): Promise<void> {
+    if (this.socket.readyState !== WebSocket.OPEN) {
+      throw new AriesFrameworkError(`${this.type} transport session has been closed.`)
+    }
+    // websockets get closed by infrastructure after a given timeout (typically 60s)
+    // this is expected and desirable, otherwise the number of opened web sockets could become unmanageable
+    // but when a mobile app becomes inactive, it stops processing websocket messages until it becomes active again
+    // as a result, messages sent whilst the app is inactive are irremediably lost when the websocket is closed
+    // in order to minimize the risk of message loss, we do a ping/pong and only send the message when the pong is received
+    let success = false;
+    let timeoutId: any | null = null;
+    const delay = (ms: number, val: any) => new Promise( (resolve) => { timeoutId = setTimeout( resolve, ms ) })
+    this.socket.once("pong", () => {
+      this.socket.send(JSON.stringify(encryptedMessage), (error?) => {
+        if (error != undefined) {
+          this.logger.error('Error sending message: ' + error)
+          throw new AriesFrameworkError(`${this.type} send message failed.`)
+        } else {
+          this.logger.debug(`${this.type} sent message successfully.`)
+          success = true;
+          clearTimeout(timeoutId)
+        }
+      })
+    })
+    this.socket.ping("ping")
+    await delay(10000, () => success = false)
+    if(!success) {
+      this.logger.error('Error pinging endpoint')
+      throw new AriesFrameworkError(`${this.type} send message failed.`)
+    }
+  }
+
+  public async close(): Promise<void> {
+    if (this.socket.readyState === WebSocket.OPEN) {
+      this.socket.close()
+    }
+  }
+}
---
+++ mediator/mediator.ts
@@ -0,0 +1,139 @@
+/**
+ * This file contains a sample mediator. The mediator supports both
+ * HTTP and WebSockets for communication and will automatically accept
+ * incoming mediation requests.
+ *
+ * You can get an invitation by going to '/invitation', which by default is
+ * http://localhost:3001/invitation
+ *
+ * To connect to the mediator from another agent, you can set the
+ * 'mediatorConnectionsInvite' parameter in the agent config to the
+ * url that is returned by the '/invitation/ endpoint. This will connect
+ * to the mediator, request mediation and set the mediator as default.
+ */
+
+import type { InitConfig } from '@aries-framework/core'
+import type { Socket } from 'net'
+
+import express from 'express'
+import { Server } from 'ws'
+
+import {
+  ConnectionsModule,
+  MediatorModule,
+  HttpOutboundTransport,
+  Agent,
+  ConnectionInvitationMessage,
+  LogLevel,
+  WsOutboundTransport,
+} from '@aries-framework/core'
+import {
+  AskarModule,
+  AskarWalletPostgresConfig,
+  AskarWalletPostgresCredentials,
+  AskarWalletPostgresStorageConfig
+} from '@aries-framework/askar'
+import { ariesAskar } from '@hyperledger/aries-askar-nodejs'
+import { HttpInboundTransport, agentDependencies } from '@aries-framework/node'
+import MediatorLogger from "./logger";
+import {WsInboundTransport} from "./WsInboundTransport";
+
+const port = process.env.AGENT_PORT ? Number(process.env.AGENT_PORT) : 3001
+
+// We create our own instance of express here. This is not required
+// but allows use to use the same server (and port) for both WebSockets and HTTP
+const app = express()
+const socketServer = new Server({ noServer: true })
+
+const endpoints = process.env.AGENT_ENDPOINTS?.split(',') ?? [`http://localhost:${port}`, `ws://localhost:${port}`]
+
+const logger = new MediatorLogger(LogLevel.debug)
+
+const db_host = process.env.POSTGRESQL_HOST || "localhost"
+const db_port = process.env.POSTGRESQL_PORT ? Number(process.env.POSTGRESQL_PORT) : 5432
+logger.info(`Using PostgreSQL db @${db_host}:${db_port}`)
+
+const postgresServerConfig: AskarWalletPostgresConfig = {
+  host: `${db_host}:${db_port}`,
+  connectTimeout: 5
+}
+
+const postgresCredentialsConfig: AskarWalletPostgresCredentials = {
+  account: process.env.POSTGRESQL_USER || "postgres",
+  password: process.env.POSTGRESQL_PASSWORD || "postgres" /*,
+  adminAccount: "postgres",
+  adminPassword: "postgres" */
+}
+
+const postgresStorageConfig: AskarWalletPostgresStorageConfig = {
+  type: 'postgres',
+  config: postgresServerConfig,
+  credentials: postgresCredentialsConfig
+}
+
+const agentConfig: InitConfig = {
+  endpoints,
+  label: process.env.AGENT_LABEL || 'Aries Framework JavaScript Mediator',
+  walletConfig: {
+    id: process.env.POSTGRESQL_DBNAME || process.env.WALLET_NAME || 'pocket-mediator-pgdb',
+    key: process.env.WALLET_KEY || 'AriesFrameworkJavaScript',
+    storage: postgresStorageConfig
+  },
+
+  logger,
+}
+
+// Set up agent
+const agent = new Agent({
+  config: agentConfig,
+  dependencies: agentDependencies,
+  modules: {
+    askar: new AskarModule( { ariesAskar }),
+    mediator: new MediatorModule({
+      autoAcceptMediationRequests: true,
+    }),
+    connections: new ConnectionsModule({
+      autoAcceptConnections: true,
+    }),
+  },
+})
+const config = agent.config
+
+// Create all transports
+const httpInboundTransport = new HttpInboundTransport({ app, port })
+const httpOutboundTransport = new HttpOutboundTransport()
+const wsInboundTransport = new WsInboundTransport({ server: socketServer })
+const wsOutboundTransport = new WsOutboundTransport()
+
+// Register all Transports
+agent.registerInboundTransport(httpInboundTransport)
+agent.registerOutboundTransport(httpOutboundTransport)
+agent.registerInboundTransport(wsInboundTransport)
+agent.registerOutboundTransport(wsOutboundTransport)
+
+// Allow to create invitation, no other way to ask for invitation yet
+httpInboundTransport.app.get('/invitation', async (req, res) => {
+  if (typeof req.query.c_i === 'string') {
+    const invitation = ConnectionInvitationMessage.fromUrl(req.url)
+    res.send(invitation.toJSON())
+  } else {
+    const { outOfBandInvitation } = await agent.oob.createInvitation()
+    const httpEndpoint = config.endpoints.find((e) => e.startsWith('http'))
+    res.send(outOfBandInvitation.toUrl({ domain: httpEndpoint + '/invitation' }))
+  }
+})
+
+const run = async () => {
+  await agent.initialize()
+
+  // When an 'upgrade' to WS is made on our http server, we forward the request to the WS server
+  // on(event: 'upgrade', listener: (req: InstanceType<Request>, socket: stream.Duplex, head: Buffer) => void): this;
+  httpInboundTransport.server?.on('upgrade', (request, socket, head) => {
+    socketServer.handleUpgrade(request, socket as Socket, head, (socket) => {
+      socketServer.emit('connection', socket, request)
+    })
+    return socketServer
+  })
+}
+
+void run()
